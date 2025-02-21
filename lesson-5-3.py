58bade2a4e6b291cac8f63156f8529ba9a549063
kristiczk <khrystyna.chychyk@badbeat.com>
2025-02-21 22:58:16 +0200

    upd

---
+++ lesson-5-3.py
@@ -0,0 +1,28 @@
+"""Користувач вводить рядок, Ваше завдання – перетворити рядок на hashtag.
+
+Декілька правил:
+
+ніяких символів з набору string.punctuation не повинно бути, у тому числі й пробілів;
+підсумкова довжина hashtag має бути не більше 140 символів.
+кожне слово починається з великої літери.
+якщо довжина фінішного хештегу більше 140 символів - обрізати підсумковий рядок до 140 символів."""
+
+# 'Python Community' -> #PythonCommunity
+# 'i like python community!' -> #ILikePythonCommunity
+# 'Should, I. subscribe? Yes!' -> #ShouldISubscribeYes
+
+import string
+
+example_string = "Should, I. subscribe? Yes!"
+string_limit = 140
+
+title_string = example_string.title()
+
+no_punctuation_spaces = "#" + "".join(
+    char for char in title_string if char not in string.punctuation and char != " "
+)
+
+if len(no_punctuation_spaces) > string_limit:
+    no_punctuation_spaces = no_punctuation_spaces[:string_limit]
+
+print(no_punctuation_spaces)
