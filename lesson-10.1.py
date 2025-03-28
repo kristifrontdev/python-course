1d0de98c6bda98dccdda73f688701b26f72285e1
kristiczk <khrystyna.chychyk@badbeat.com>
2025-03-28 13:37:27 +0200

    upd

---
+++ lesson-10.1.py
@@ -0,0 +1,22 @@
+from inspect import isgenerator
+
+
+def pow(x):
+    return x**2
+
+
+def some_gen(begin, length, func):
+    res = begin
+    counter = 0
+    while counter < length:
+        yield res
+        res = pow(res)
+        counter += 1
+
+
+gen = some_gen(2, 4, pow)
+print(list(gen))
+
+# assert isgenerator(gen) == True, "Test1"
+# assert list(gen) == [2, 4, 16, 256], "Test2"
+# print("OK")
