294f0848cf53dabd225f4b7d778f52a655d1b82c
kristiczk <khrystyna.chychyk@badbeat.com>
2025-06-15 17:29:43 +0000

    test

--- python/lesson-15-2.py
+++ python/lesson-15-2.py
@@ -38,26 +38,28 @@ class Fraction:
         return f"Fraction: {self.a}, {self.b}"
 
 f_a = Fraction(2, 3)
-f_b = Fraction(3, 6)
+f_b = Fraction(1, 3)
 
 f_c = f_b + f_a
 # print(f_c)
 # assert str(f_c) == 'Fraction: 21, 18'
 f_d = f_b * f_a
-assert str(f_d) == 'Fraction: 6, 18'
+# print(f_d)
+# assert str(f_d) == 'Fraction: 6, 18'
 
 f_e = f_a - f_b
-print(f_e)
+# print(f_e)
 # assert str(f_e) == 'Fraction: 3, 18'
 
-assert f_d < f_c  # True
 print(f_d < f_c)
+assert f_d < f_c  # True
 
-assert f_d > f_e  # True
 print(f_d > f_e)
+assert f_d > f_e  # True
 
-assert f_a != f_b  # True
 print(f_a != f_b)
+# assert f_a != f_b  # True
+# print(f_a != f_b)
 
 f_1 = Fraction(2, 4)
 f_2 = Fraction(3, 6)
